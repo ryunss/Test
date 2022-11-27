906722b86b72609735dcba6c3075597746ecfc37
jinharyun <ajdaduy@naver.com>
2022-11-27 09:24:12 +0900

    OOP

---
+++ JavaWork/Lec16_OOP/src/com/lec/java/oop01/Polymorphism01Main.java
@@ -0,0 +1,91 @@
+package com.lec.java.oop01;
+/*
+ 	다형성  ( Polymorphism )
+ 	하나의 이름의 클래스나 메소드가 '여러 가지 형태의 동작을 하는 능력'
+ 	
+ 	클래스의 다형성:
+	 	한 타입의 참조변수로 여러타입의 객체를 참조 가능.
+	 	조상클래스 타입의 참조변수로 자손클래스의 인스턴스를 참조가능한것
+ 	
+ 	메소드의 다형성:
+ 		메소드 오버로딩, 메소드 오버라이딩
+ 	
+ */
+public class Polymorphism01Main {
+
+	public static void main(String[] args) {
+		System.out.println("다형성(Polymorphism)");
+		
+		System.out.println();
+		
+		// v1, c1, h1 의 타입은 다르다
+		// 각각의 타입에 맞는 인스턴스 생성한뒤 대입 (참조하게 했다)
+		Vehicle v1 = new Vehicle();
+		Car c1 = new Car();
+		HybridCar h1 = new HybridCar();
+		
+		// 각각의 타입에 오버라이딩 된 메소드가 동작함
+		v1.displayInfo();
+		c1.displayInfo();
+		h1.displayInfo();
+		
+		System.out.println("==============");
+		
+		Vehicle car1 = new Car(); // 조상 <- 자손 (가능)
+		Vehicle car2 = new HybridCar(); // 조상 <- 자손 (가능)
+		Car car3 = new HybridCar(); // 조상 <- 자손 (가능)
+		
+//		HybridCar car7 = new Vehicle(); // 자손 <- 조상 (불가)
+		
+		// car1~car3 변수 타입에 관계에 상관없이
+		// 오버라이딩 된 메소드가 '알아서' 동작한다.
+		car1.displayInfo();
+		car2.displayInfo();
+		car3.displayInfo();
+		
+		System.out.println("\n 프로그램 종료");
+	} // end main()
+	
+	// TODO
+
+} // end class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JavaWork/Lec16_OOP/src/com/lec/java/oop05/Interface01Main.java
@@ -0,0 +1,129 @@
+package com.lec.java.oop05;
+
+/*
+ 인터페이스(interface):
+ 1. 모든 메소드가 public abstract으로 선언되고,
+ 2. 모든 멤버 변수가 public static final로 선언된
+ 특별한 종류의 추상 클래스
+
+ 인터페이스는 interface라고 선언
+ 인터페이스를 구현(상속)하는 클래스에서는 implements 키워드를 사용
+ 인터페이스를 구현(상속)할 때는 개수 제한이 없다. (다중상속)
+ 메소드 선언에서 public abstract는 생략 가능
+ 멤버 변수 선언에서 public static final은 생략 가능
+*/
+
+public class Interface01Main {
+
+	public static void main(String[] args) {
+		System.out.println("인터페이스(interface)");
+		
+		TestImpl test1 = new TestImpl();
+		test1.testAAA();
+		test1.testBBB();
+		
+		System.out.println("==============");
+		TestImpl2 test2 = new TestImpl2();
+		test2.testAAA();
+		test2.testBBB();
+		test2.testCCC();
+
+//		System.out.println(test2.MIN); // 다중상속의 문제점@!
+		System.out.println(TestInterface.MIN);	// static 인점을 활용해서 클래스.ㅇㅇ 으로 사용한다!		
+		System.out.println(TestInterface2.MIN);
+		
+		System.out.println("\n 프로그램 종료");
+	} // end main()
+
+} // end class
+
+interface TestInterface{
+	// 모든 멤버 변수가 public static final로 선언되어야
+	public static final int MIN = 0;
+	int MAX = 100; // public static final 생략됨. 생략해도 가능.
+//	private static final String JAVA_STRING = "Java"; // 에러. private 에러임. public 만 가능!!!
+	public static final String JAVA_STRING = "Java";
+	
+	// 모든 메소드는 public abstract로 선언되어야
+	public abstract void testAAA();
+	void testBBB(); // public abstract 생략가능.
+
+}
+interface TestInterface2 {
+	public static final int MIN = 1;
+	
+	public abstract void testAAA();
+	public abstract int testCCC();
+}
+class TestImpl implements TestInterface{
+
+	@Override
+	public void testAAA() {
+		System.out.println("testAAA");
+	}
+
+	@Override
+	public void testBBB() {
+		System.out.println("testBBB");
+	}
+	
+}
+
+// 인터페이스는 다중상속 가능
+class TestImpl2 implements TestInterface, TestInterface2{ 
+	// 시그니쳐(이름)가 겹치기 때문에 3개가 오버라이딩됨.
+
+	@Override
+	public int testCCC() {
+		System.out.println("testCCC");
+		return 0;
+	}
+
+	// 양쪽 부모의 동일 시그니쳐 추상메소드는 하나만 오버라이딩 된다.
+	@Override
+	public void testAAA() {
+		System.out.println("testAAA");
+	}
+
+	@Override
+	public void testBBB() {
+		System.out.println("testBBB");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
