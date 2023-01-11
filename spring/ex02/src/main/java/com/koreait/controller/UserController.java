e8a7c98a24ba65f47a77e2e4945f73ae19ec8588
jinharyun <ajdaduy@naver.com>
2023-01-11 11:04:50 +0900

    controller 테스트

--- spring/ex02/src/main/java/com/koreait/controller/UserController.java
+++ spring/ex02/src/main/java/com/koreait/controller/UserController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.koreait.domain.UserDTO;
 import com.koreait.service.UserService;
 
+import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
 @RequestMapping("/user/*")
 public class UserController {
+	@Setter(onMethod_ = @Autowired)
 	private UserService service;
 	
 //	@GetMapping("/join")
